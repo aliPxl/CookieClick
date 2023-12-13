4fa287771f02ea74d0aa490e7f57fae3a16cab97
ali mocro <12202425@student.pxl.be>
2023-12-13 12:00:53 +0100

    Cookie-Clicker Refactor, 19 ik heb heel de aplicatie gerefectord, de eerste ittiratie zit hier bijna  helemaal in behalve 16 en 3 extra buttons van itteratie 2 issue key 19

--- CookieClick/CookieClick/MainWindow.xaml.cs
+++ CookieClick/CookieClick/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace CookieClick
 {
@@ -20,192 +21,264 @@ namespace CookieClick
     /// </summary>
     public partial class MainWindow : Window
     {
-        //score variable double
+        DispatcherTimer timer;
         double ScoreVar = 0;
+        int ButtonClicked = 0;
+        //double PasiveIncomenVar = 0;
+        //                                               BUTTONS
+        //cursor
+        private const double BasisPrijsCursor = 15;
+        double CursorAantalVar;
+        private const double CursorPerSeconde=0.1;
 
-        //Cursor
-        //prijs Cursor
-        double CursorPrijs = 15.00;
-        //cursor per seconden
-        double CursorSeconde=0.1;
-        //count om bij te houden hoevaak de speler deze investering heeft gekocht hoeveel de speler hee
-        int CursorAantal = 0;
-
-        //Grandma
-        // prijs Grandma
-        double GrandmaPrijs = 2;// 100.00;
-        //grandma per seconde
-        double GrandmaSeconde = 1.0;
-        //count om bij te houden hoevaak de speler deze investering heeft gekocht hoeveel de speler hee
-        int GrandmaAantal = 0;
+        //Granma
+        private const double BasisPrijsGrandma = 16;
+        public int GrandmaAantalVar;
+        private const double GrandmaPerSeconde =1.0;
 
         //farm
-        //prijs Farm
-        double FarmPrijs = 3;//1100.00;
-        //farm per seconde
-        double FarmSeconde = 8.0;
-        //count om bij te houden hoevaak de speler deze investering heeft gekocht hoeveel de speler hee
-        int FarmAantal = 0;
+        private const double BasisPrijsFarm = 17;
+        public int FarmAantalVar;
+        private const double FarmPerSeconde = 8.0;
+        //Mine
+        private const double BasisPrijsMine = 18;
+        public int MineAantalVar;
+        private const double MinePerSeconde = 47.0;
 
-        //mine
-        //prijs Mine
-        double MinePrijs = 4;//1200.00;
-        //mine per seconde
-        double MineSeconde = 47.0;
-        //count om bij te houden hoevaak de speler deze investering heeft gekocht hoeveel de speler hee
-        int MineAantal = 0;
+        //Factory
+        private const double BasisPrijsFactory = 19;
+        public int FactoryAantalVar;
+        private const double FactoryPerSeconde =260.0;
+
+        //Bank
+        private const double BasisPrijsBank = 20;
+        public int BankAantalVar;
+        private const double BankPerSeconde =1400.0;
+        //Temple
+        private const double BasisPrijsTemple = 21;
+        public int TempleAantalVar;
+        private const double TemplePerSeconde =7800.0;
+
+        /// <summary>
+        /// nieuwe timer word gedeclareerd 
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-            //score Update functie word opgeroepen deze zorgt ervoor dat de score aan de gebruiker word getoond
-            ScoreUpdate();
+            timer = new DispatcherTimer();
+            timer.Tick += TickerUpdate;
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Start();
         }
-        //eigen functies 
-        //functie om te checken welke button enabled kan worden
-        private void EnableOrDisable()
+        //TickerUpdate word om de 10 miliseconde gedaan
+        private void TickerUpdate(object sender, EventArgs e)
         {
-            ///cursor groter dan de prijs dan word button enabled 
-            if (ScoreVar >= CursorPrijs)
-            {
-                Cursor.IsEnabled = true;
-                ScoreUpdate();
-            }
-            //zodra deze lager gaat word de button weer disabled 
-            else if (ScoreVar < CursorPrijs)
-            {
-                Cursor.IsEnabled = false; 
-                ScoreUpdate();
-            }
+            // ScoreVar += alle verschillende passive inkomsten;
 
-            //Grandam groter dan de prijs dan word button enabled
-            if (ScoreVar >= GrandmaPrijs)
-            {
-                Grandma.IsEnabled = true;
-                ScoreUpdate();
+            //for elke soort inkomsten
+            //    verhoog score op basis van hoeveelheid cursors/grandmas/... * inkomst van cursor/grandmas/...
 
-            }
-            //zodra deze lager gaat word de button weer disabled 
-            else if (ScoreVar < GrandmaPrijs)
-            {
-                Grandma.IsEnabled = false;
-                ScoreUpdate();
-            }
-
-            //Farm groter dan de prijs dan word button enabled
-            if (ScoreVar >= FarmPrijs)
-            {
-                Farm.IsEnabled = true;
-            }
-            //zodra deze lager gaat word de button weer disabled 
-            else if (ScoreVar <= FarmPrijs)
-            {
-                Farm.IsEnabled = false;
-                ScoreUpdate();
-            }
-
-            //Mine groter dan de prijs dan word button enabled
-            if (ScoreVar >= MinePrijs)
-            {
-                Mine.IsEnabled = true;
-            }
-            //zodra deze lager gaat word de button weer disabled 
-            else if (ScoreVar <= MinePrijs)
-            {
-                Mine.IsEnabled = false;
-                ScoreUpdate();
-            }
-
+            UpdateFunctie();
         }
-        //scoreUpdate functie
-        private void ScoreUpdate()
+
+        //                                  koekjes_klick
+        private void Koekje_Click(object sender, RoutedEventArgs e)
         {
-       //scorevar word nog altijd opgeslagen als commagetal maar word in de volgende relel naar onder afgerond
-       //om aan de geburiker te laten zien
-            ScoreVar=Math.Floor(ScoreVar);
-            //scorelbl word opgevuld met de score 
-            ScoreLbl.Content = $"score: {ScoreVar}";
-            //tietel word ook geUpdate MW is de name van mij MainWindow
-            MW.Title = $"{ScoreVar}";
-
-            /////////////////////////////////////////////////////////////////buttons 
-            //Cursor
-            LblCursorPrijs.Content = $"{CursorPrijs}";
-            LblCursorAantal.Content = $"{CursorAantal}";
-            Cursor.ToolTip= $"Aantal cookies per seconde: {CursorSeconde}";
-
-            //Grandma
-            LblGrandmaPrijs.Content = $"{GrandmaPrijs}";
-            LblGrandmaAantal.Content = $"{GrandmaAantal}";
-            Grandma.ToolTip = $"Aantal cookies per seconde: {GrandmaSeconde}";
-
-            //Farm
-              LblFarmPrijs.Content = $"{FarmPrijs}";
-              LblFarmAantal.Content = $"{FarmAantal}";
-              Farm.ToolTip = $"Aantal cookies per seconde: {FarmSeconde}";
-
-            //Mine
-            LblMinePrijs.Content = $"{MinePrijs}";
-            LblMineAantal.Content = $"{MineAantal}";
-            Mine.ToolTip = $"Aantal cookies per seconde: {MineSeconde}";
+            ScoreVar++;
         }
-        //mouseDown event koekje
+
         private void KoekjeImg_MouseDown(object sender, MouseButtonEventArgs e)
         {
             //koekje word kleiner 
             KoekjeImg.Width = 130;
             //score word met 1 verhoogd
             ScoreVar++;
-            //score Update functie word opgeroepen deze zorgt ervoor dat de score aan de gebruiker word 
-            ScoreUpdate();
-            EnableOrDisable();
         }
-        //mouseUp event koekje
+
         private void KoekjeImg_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            //koekje original size
             KoekjeImg.Width = 150;
-            //score Update functie word opgeroepen deze zorgt ervoor dat de score aan de gebruiker word getoond
-            ScoreUpdate();
         }
 
         private void KoekjeImg_MouseLeave(object sender, MouseEventArgs e)
         {
-            //koekje original size
             KoekjeImg.Width = 150;
-            //score Update functie word opgeroepen deze zorgt ervoor dat de score aan de gebruiker word getoond
-            ScoreUpdate();
+        }
+        private void UpdateFunctie()
+        {
+            CanInvest();
+
+            //score word afgerond en getoond in de lable Boven 
+            LblScore.Content = Math.Round(ScoreVar, 1);
+            MW.Title = ScoreVar.ToString();
+
+            //nieuwe button !!!!!!!!!!!!!!
+            //cursor        prijs in de button word eerst berekend en opgevuld, en aantal keren gekocht 
+            LblCursorPrijs.Content = prijsBerekenen(BasisPrijsCursor, CursorAantalVar);
+            LblCursorAantal.Content = CursorAantalVar;
+            CursorBtn.ToolTip =$"opbrengst per seconde: {CursorPerSeconde}";
+            //Grandma       prijs in de button word eerst berekend en opgevuld, en aantal keren gekocht 
+            LblGrandmaPrijs.Content = prijsBerekenen(BasisPrijsGrandma, GrandmaAantalVar);
+            LblGrandmaAantal.Content = GrandmaAantalVar;
+            GrandmaBtn.ToolTip = $"opbrengst per seconde: {GrandmaPerSeconde}";  
+            //farm          prijs in de button word eerst berekend en opgevuld, en aantal keren gekocht 
+            LblFarmPrijs.Content = prijsBerekenen(BasisPrijsFarm, FarmAantalVar); ;
+            LblFarmAantal.Content = FarmAantalVar;
+            FarmBtn.ToolTip = $"opbrengst per seconde: {FarmPerSeconde}";
+            //Mine          prijs in de button word eerst berekend en opgevuld, en aantal keren gekocht 
+            LblMinePrijs.Content = prijsBerekenen(BasisPrijsMine, MineAantalVar); ;
+            LblMineAantal.Content = MineAantalVar;
+            MineBtn.ToolTip = $"opbrengst per seconde: {MinePerSeconde}"; ;
+            //Factory          prijs in de button word eerst berekend en opgevuld, en aantal keren gekocht 
+            LblFactoryPrijs.Content = prijsBerekenen(BasisPrijsFactory, FactoryAantalVar); ;
+            LblFactoryAantal.Content = FactoryAantalVar;
+            FactoryBtn.ToolTip = $"opbrengst per seconde: {FactoryPerSeconde}"; ;
+            //Bank          prijs in de button word eerst berekend en opgevuld, en aantal keren gekocht 
+            LblBankPrijs.Content = prijsBerekenen(BasisPrijsBank, BankAantalVar); ;
+            LblBankAantal.Content = BankAantalVar;
+            BankBtn.ToolTip= $"opbrengst per seconde: {BankPerSeconde}"; ;
+            //Temple          prijs in de button word eerst berekend en opgevuld, en aantal keren gekocht 
+            LblTemplePrijs.Content = prijsBerekenen(BasisPrijsTemple, TempleAantalVar); ;
+            LblTempleAantal.Content = TempleAantalVar;
+            TempleBtn.ToolTip = $"opbrengst per seconde: {TemplePerSeconde}"; ;
         }
 
-        private void Cursor_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// deze functie gaat kijken of ik genoeg koekjes heb om te investering, zodra deze genoeg is word button 
+        /// enabled of indien niet genoeg word de button disabled, de basisprijs en het aantal keer gekocht word 
+        /// megegeven als parameter om de nieuwe prijs te berekenen en returnt de nieuwprijs
+        /// </summary>
+        private void CanInvest()
         {
-            CursorAantal++;
-            ScoreVar -= CursorPrijs;
-            ScoreUpdate();
-            EnableOrDisable();
+            //Cursor
+            if (prijsBerekenen(BasisPrijsCursor, CursorAantalVar) > ScoreVar)
+            {
+                CursorBtn.IsEnabled = false;
+            }
+            else
+            {
+                CursorBtn.IsEnabled = true;
+            }
+            //Grandma
+
+            if (prijsBerekenen(BasisPrijsGrandma, GrandmaAantalVar) > ScoreVar)
+            {
+                GrandmaBtn.IsEnabled = false;
+            }
+            else
+            {
+                GrandmaBtn.IsEnabled = true;
+            }
+            //Farm
+            if (prijsBerekenen(BasisPrijsFarm, FarmAantalVar) > ScoreVar)
+            {
+                FarmBtn.IsEnabled = false;
+            }
+            else
+            {
+                FarmBtn.IsEnabled = true;
+            }
+            //Mine
+            if (prijsBerekenen(BasisPrijsMine, MineAantalVar) > ScoreVar)
+            {
+                MineBtn.IsEnabled = false;
+            }
+            else
+            {
+                MineBtn.IsEnabled = true;
+            }
+
+            //Factory
+            if (prijsBerekenen(BasisPrijsFactory, FactoryAantalVar) > ScoreVar)
+            {
+                FactoryBtn.IsEnabled = false;
+            }
+            else
+            {
+                FactoryBtn.IsEnabled = true;
+            }
+
+            //Bank
+            if (prijsBerekenen(BasisPrijsBank, BankAantalVar) > ScoreVar)
+            {
+                BankBtn.IsEnabled = false;
+            }
+            else
+            {
+                BankBtn.IsEnabled = true;
+            }
+
+            //Temple
+            if (prijsBerekenen(BasisPrijsTemple, TempleAantalVar) > ScoreVar)
+            {
+                TempleBtn.IsEnabled = false;
+            }
+            else
+            {
+                TempleBtn.IsEnabled = true;
+            }
         }
 
-        private void Grandma_Click(object sender, RoutedEventArgs e)
+        // voor elke button geld het zelfde principe, dus elke button luistert naar een event 
+        // aantal van die investering gaat omhoog,
+        // score gaat omlaag met prijs van investering 
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            GrandmaAantal++;
-            ScoreVar -= GrandmaPrijs;
-            ScoreUpdate();
-            EnableOrDisable();
+            string ButtonName = (sender as Button).Name;
+            switch (ButtonName)
+            {
+                //Cursor
+                case "CursorBtn":
+                    CursorAantalVar++;
+                    ScoreVar -= double.Parse(LblCursorPrijs.Content.ToString());
+                    break;
+                //Grandma
+                case "GrandmaBtn":
+                    GrandmaAantalVar++;
+                    ScoreVar -= double.Parse(LblGrandmaPrijs.Content.ToString());
+                    break;
+                //Farm
+                case "FarmBtn":
+                    FarmAantalVar++;
+                    ScoreVar -= double.Parse(LblFarmPrijs.Content.ToString());
+                    break;
+                //Mine
+                case "MineBtn":
+                    MineAantalVar++;
+                    ScoreVar -= double.Parse(LblMinePrijs.Content.ToString());
+                    break;
+                //Factory
+                case "FactoryBtn":
+                    FactoryAantalVar++;
+                    ScoreVar -= double.Parse(LblFactoryPrijs.Content.ToString());
+                    break;
+                //Bank
+                case "BankBtn":
+                    BankAantalVar++;
+                    ScoreVar -= double.Parse(LblBankPrijs.Content.ToString());
+                    break;
+                //Temple
+                case "TempleBtn":
+                    TempleAantalVar++;
+                    ScoreVar -= double.Parse(LblTemplePrijs.Content.ToString());
+                    break;
+            }
+
         }
 
-        private void Farm_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// de formule van deze functie => standaardprijs maal (1.15 tot de macht van aantal) 
+        /// </summary>
+        /// <param name="standaardprijs">standaardprijs van mijn investering cursorprijs,grandmaprijs,farmprijs,minePrijs</param>
+        /// <param name="aantal"> aantal keer gekocht </param>
+        /// <returns> de berekende prijs </returns>
+        private double prijsBerekenen(double standaardprijs, double aantal)
         {
-            FarmAantal++;
-            ScoreVar -= FarmPrijs;
-            ScoreUpdate();
-            EnableOrDisable();
+            double NieuwPrijs = Math.Ceiling(standaardprijs * Math.Pow(1.15, aantal));
+            return NieuwPrijs;
+
         }
 
-        private void Mine_Click(object sender, RoutedEventArgs e)
-        {
-            MineAantal++;
-            ScoreVar -= MinePrijs;
-            ScoreUpdate();
-            EnableOrDisable();
-        }
+
     }
 }
